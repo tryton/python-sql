939099c99ca6d5c1ab3c8dbf6a770160478d785e
Cédric Krier <ced@b2ck.com>
2012-04-26 20:13:42 +0200

    80 cols even for classifiers

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup(name='python-sql',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: '
+        'GNU Library or Lesser General Public License (LGPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
