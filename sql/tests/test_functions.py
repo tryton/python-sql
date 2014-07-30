04e0c699439d5470a84aba0f32a69f2acf45ec21
Cédric Krier <ced@b2ck.com>
2014-07-31 00:19:46 +0200

    Fix Mapping in Trim function and increase test coverage of functions

--- sql/tests/test_functions.py
+++ sql/tests/test_functions.py
@@ -29,7 +29,8 @@
 import unittest
 
 from sql import Table, Flavor
-from sql.functions import Function, Abs, Overlay, Trim, AtTimeZone, Div
+from sql.functions import (Function, FunctionKeyword, FunctionNotCallable, Abs,
+    Overlay, Trim, AtTimeZone, Div, CurrentTime)
 
 
 class TestFunctions(unittest.TestCase):
@@ -49,14 +50,40 @@ class TestFunctions(unittest.TestCase):
             _function = 'MY_ABS'
             params = ('test',)
 
+        class MyOverlay(FunctionKeyword):
+            _function = 'MY_OVERLAY'
+            _keywords = ('', 'PLACING', 'FROM', 'FOR')
+
+        class MyCurrentTime(FunctionNotCallable):
+            _function = 'MY_CURRENT_TIME'
+
+        class MyTrim(Trim):
+            _function = 'MY_TRIM'
+
         abs_ = Abs(self.table.c1)
+        overlay = Overlay(self.table.c1, 'test', 2)
+        current_time = CurrentTime()
+        trim = Trim(' test ')
         flavor = Flavor(function_mapping={
                 Abs: MyAbs,
+                Overlay: MyOverlay,
+                CurrentTime: MyCurrentTime,
+                Trim: MyTrim,
                 })
         Flavor.set(flavor)
         try:
             self.assertEqual(str(abs_), 'MY_ABS("c1")')
             self.assertEqual(abs_.params, ('test',))
+
+            self.assertEqual(str(overlay),
+                'MY_OVERLAY("c1" PLACING %s FROM %s)')
+            self.assertEqual(overlay.params, ('test', 2))
+
+            self.assertEqual(str(current_time), 'MY_CURRENT_TIME')
+            self.assertEqual(current_time.params, ())
+
+            self.assertEqual(str(trim), 'MY_TRIM(BOTH %s FROM %s)')
+            self.assertEqual(trim.params, (' ', ' test ',))
         finally:
             Flavor.set(Flavor())
 
@@ -73,6 +100,10 @@ class TestFunctions(unittest.TestCase):
         trim = Trim(' test ')
         self.assertEqual(str(trim), 'TRIM(BOTH %s FROM %s)')
         self.assertEqual(trim.params, (' ', ' test ',))
+
+        trim = Trim(self.table.c1)
+        self.assertEqual(str(trim), 'TRIM(BOTH %s FROM "c1")')
+        self.assertEqual(trim.params, (' ',))
 
     def test_at_time_zone(self):
         time_zone = AtTimeZone(self.table.c1, 'UTC')
@@ -99,3 +130,8 @@ class TestFunctions(unittest.TestCase):
                 self.table.c1 // self.table.c2]:
             self.assertEqual(str(div), 'DIV("c1", "c2")')
             self.assertEqual(div.params, ())
+
+    def test_current_time(self):
+        current_time = CurrentTime()
+        self.assertEqual(str(current_time), 'CURRENT_TIME')
+        self.assertEqual(current_time.params, ())
