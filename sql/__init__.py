9dd6198dc1dcf6a57edecbfd16a48b8bb194cfd5
Cédric Krier <ced@b2ck.com>
2025-05-02 21:54:13 +0200

    Prepare release

--- sql/__init__.py
+++ sql/__init__.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 from itertools import chain
 from threading import current_thread, local
 
-__version__ = '1.5.3'
+__version__ = '1.6.0'
 __all__ = [
     'Flavor', 'Table', 'Values', 'Literal', 'Column', 'Grouping', 'Conflict',
     'Matched', 'MatchedUpdate', 'MatchedDelete',
